40f37718af657c5ec5678401d029f63ca4083dc5
newNathaporn <new_ziizyy@hotmail.co.th>
2017-11-09 12:01:46 +0700

    code ที่รวม class สำหรับ user story "Member" ที่เก็บรายชื่อสมาชิกในกลุ่ม ผ่านการรัน JUNIT แล้ว

---
+++ Labgit/src/controller/systemOut.java
@@ -0,0 +1,15 @@
+package controller;
+
+public class systemOut {
+
+	public static void main(String[] args) {
+		String[] name = {"5909650177 Nunun","5909650227 Ongfong","5909650730 Mosper","5909650771 Poppy","5909650813 Newzii"};
+		System.out.println("fivePinkyPinky\n");
+		for (int i = 0; i < name.length; i++) {
+			
+			System.out.println(" "+name[i]);
+		}
+		System.out.println("\nSec.650002");
+	}
+
+}
